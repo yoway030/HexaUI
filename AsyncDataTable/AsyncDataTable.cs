2d9749a6d306f9aa69e75ce4fbaca10fbd143c09
김용환 <yoway030@hotmail.com>
2025-11-24 00:53:29 +0900

    새로운 AsyncDataTable 프로젝트 추가
    비동기 데이터 테이블을 구현하는 `AsyncDataTable` 프로젝트가 추가되었습니다.
    `AsyncDataTable.cs` 파일에는 비동기적으로 데이터를 처리하는 클래스가 포함되어 있으며,
    명령을 큐에 추가하고 처리하는 메커니즘이 구현되었습니다.
    또한, `CmdList` 클래스가 추가되어 비동기 명령을 처리하고 데이터 목록을 관리합니다.
    `Program.cs` 파일에는 단일 스레드에서 비동기 작업을 처리하는 `SingleThreadSynchronizationContext` 및
    `SingleWorkerThread` 클래스가 추가되었습니다.
    `AsyncDataTable.csproj` 파일이 추가되어 .NET SDK를 사용한 프로젝트 구성이 포함되었으며,
    솔루션 파일(`HexaUI.sln`)에 새로운 프로젝트 관련 설정이 추가되었습니다.

---
+++ AsyncDataTable/AsyncDataTable.cs
@@ -0,0 +1,135 @@
+﻿namespace AsyncDataTable;
+
+using System;
+using System.Threading.Channels;
+
+public class AsyncDataTable<T>
+{
+    public enum Command
+    {
+        Insert,
+        Update,
+        Delete,
+        Upsert,
+        Clear,
+    }
+
+    public record struct IndexedData(int Index, T Data);
+    public record struct CommandedData(Command Command, T Item, Predicate<T>? predicate, TaskCompletionSource<int>? tcs);
+
+    public AsyncDataTable()
+    {
+        _commandChannel = Channel.CreateUnbounded<CommandedData>(
+            new UnboundedChannelOptions
+            {
+                SingleReader = true,
+                SingleWriter = false
+            });
+    }
+
+    private readonly List<IndexedData> _datas = new();
+    private int _dataLastIndex = -1;
+
+    private readonly Channel<CommandedData> _commandChannel;
+
+    public ValueTask InsertCommand(Command cmd, T data, Predicate<T>? predicate = null)
+    {
+        var cmdData = new CommandedData(cmd, data, predicate, null);
+        return _commandChannel.Writer.WriteAsync(cmdData);
+    }
+
+    public Task<int> InsertCommandAsync(Command cmd, T data, Predicate<T>? predicate = null)
+    {
+        TaskCompletionSource<int> tcs = new();
+        var cmdData = new CommandedData(cmd, data, predicate, tcs);
+        _commandChannel.Writer.WriteAsync(cmdData);
+        return tcs.Task;
+    }
+
+    public void ProcessCommand()
+    {
+        while (_commandChannel.Reader.TryRead(out var cmdData))
+        {
+            int foundIndex = -1;
+
+            try
+            {
+                switch (cmdData.Command)
+                {
+                    case Command.Insert:
+                        _datas.Add(new IndexedData(++_dataLastIndex, cmdData.Item));
+                        cmdData.tcs?.SetResult(_dataLastIndex);
+                        break;
+                    case Command.Update:
+                        if (cmdData.predicate != null)
+                        {
+                            for (int i = 0; i < _datas.Count; i++)
+                            {
+                                if (cmdData.predicate(_datas[i].Data))
+                                {
+                                    foundIndex = _datas[i].Index;
+                                    _datas[i] = new IndexedData(_datas[i].Index, cmdData.Item);
+                                }
+                            }
+                        }
+
+                        cmdData.tcs?.SetResult(foundIndex);
+                        break;
+                    case Command.Delete:
+                        if (cmdData.predicate != null)
+                        {
+                            for (int i = 0; i < _datas.Count; i++)
+                            {
+                                if (cmdData.predicate(_datas[i].Data))
+                                {
+                                    foundIndex = _datas[i].Index;
+                                    _datas.RemoveAt(i);
+                                    break;
+                                }
+                            }
+                        }
+
+                        cmdData.tcs?.SetResult(foundIndex);
+                        break;
+                    case Command.Upsert:
+                        if (cmdData.predicate != null)
+                        {
+                            bool found = false;
+                            for (int i = 0; i < _datas.Count; i++)
+                            {
+                                if (cmdData.predicate(_datas[i].Data))
+                                {
+                                    foundIndex = _datas[i].Index;
+                                    _datas[i] = new IndexedData(_datas[i].Index, cmdData.Item);
+                                    found = true;
+                                    break;
+                                }
+                            }
+
+                            if (!found)
+                            {
+                                _datas.Add(new IndexedData(++_dataLastIndex, cmdData.Item));
+                                foundIndex = _dataLastIndex;
+                            }
+                        }
+                        else
+                        {
+                            _datas.Add(new IndexedData(++_dataLastIndex, cmdData.Item));
+                            foundIndex = _dataLastIndex;
+                        }
+
+                        cmdData.tcs?.SetResult(foundIndex);
+                        break;
+                    case Command.Clear:
+                        _datas.Clear();
+                        cmdData.tcs?.SetResult(foundIndex);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                cmdData.tcs?.SetException(ex);
+            }
+        }
+    }
+}
---
+++ AsyncDataTable/CmdList.cs
@@ -0,0 +1,170 @@
+﻿namespace AsyncDataTable;
+
+using System;
+using System.Threading.Channels;
+
+public class CmdList<TData> : IAsyncDisposable, IDisposable
+    where TData : IEquatable<TData>
+{
+    public CmdList()
+    {
+        _cmdChannel = Channel.CreateUnbounded<Action>(
+            new UnboundedChannelOptions
+            {
+                SingleReader = true,
+                SingleWriter = false
+            });
+    }
+
+    private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+    private readonly Channel<Action> _cmdChannel;
+
+    private Task? _processorTask;
+
+    private readonly List<TData> _datas = new();
+
+    public void InitAsyncCommandProcessor()
+    {
+        _processorTask = Task.Run(CommandProcessor, _cts.Token);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        _cts.Cancel();
+        _cmdChannel.Writer.Complete();
+
+        if (_processorTask != null)
+        {
+            await _processorTask.ConfigureAwait(false);
+        }
+
+        _cts.Dispose();
+    }
+
+    public void Dispose()
+    {
+        _cts.Cancel();
+        _cmdChannel.Writer.Complete();
+        _processorTask?.Wait();
+        _cts.Dispose();
+    }
+
+    private async Task<TResult> CommandWithResponse<TResult>(Func<TResult> cmd)
+    {
+        var tcs = new TaskCompletionSource<TResult>();
+
+        Action cmdWrapper = () =>
+        {
+            try
+            {
+                TResult result = cmd();
+                tcs.SetResult(result);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        };
+
+        if (_cmdChannel.Writer.TryWrite(cmdWrapper) == false)
+        {
+            await _cmdChannel.Writer.WriteAsync(cmdWrapper);
+        }
+        
+        return await tcs.Task;
+    }
+
+    private ValueTask Command(Action cmd)
+    {
+        if (_cmdChannel.Writer.TryWrite(cmd))
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        return _cmdChannel.Writer.WriteAsync(cmd);
+    }
+
+    public ValueTask Add(TData data) => Command(() => AddInternal(data));
+    public ValueTask Update(int index, TData data) => Command(() => UpdateInternal(index, data));
+    public ValueTask Upsert(TData data) => Command(() => UpsertInternal(data)); 
+    public ValueTask RemoveAt(int index) => Command(() => RemoveAtInternal(index)); 
+    public ValueTask Clear() => Command(() => _datas.Clear()); 
+
+    public Task<int> AddWithResponse(TData data) => CommandWithResponse(() => AddInternal(data));
+    public Task<int> UpdateWithResponse(int index, TData data) => CommandWithResponse(() => UpdateInternal(index, data));
+    public Task<int> UpsertWithResponse(TData data) => CommandWithResponse(() => UpsertInternal(data));
+    public Task<int> RemoveAtWithResponse(int index) => CommandWithResponse(() => RemoveAtInternal(index));
+    public Task<TData> GetWithResponse(int index) => CommandWithResponse(() => GetInternal(index));
+    public Task<IReadOnlyList<TData>> CopyWithResponse() => CommandWithResponse(() => CopyInternal());
+    public Task ClearWithResponse()
+    {
+        return CommandWithResponse(() =>
+        {
+            ClearInternal();
+            return Task.CompletedTask;
+        });
+    }
+
+    private async Task CommandProcessor()
+    {
+        try
+        {
+            await foreach (var cmd in _cmdChannel.Reader.ReadAllAsync(_cts.Token))
+            {
+                cmd();
+            }
+        }
+        catch(OperationCanceledException)
+        {
+            // 정상 종료
+        }
+    }
+
+    private int AddInternal(TData data)
+    {
+        _datas.Add(data);
+        return _datas.Count - 1;
+    }
+
+    private int UpdateInternal(int index, TData data)
+    {
+        _datas[index] = data;
+        return index;
+    }
+
+    private int RemoveAtInternal(int index)
+    {
+        _datas.RemoveAt(index);
+        return index;
+    }
+
+    private TData GetInternal(int index)
+    {
+        return _datas[index];
+    }
+
+    private int UpsertInternal(TData data)
+    {
+        int index = _datas.FindIndex(d => d.Equals(data));
+        if (index >= 0)
+        {
+            _datas[index] = data;
+            return index;
+        }
+        else
+        {
+            _datas.Add(data);
+            return _datas.Count - 1;
+        }
+    }
+
+    private void ClearInternal()
+    {
+        _datas.Clear();
+    }
+
+    private IReadOnlyList<TData> CopyInternal()
+    {
+        return _datas.ToList();
+    }
+}
