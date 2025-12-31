bffdca7c4b25d015b5c4521571e1a3c984c24d1e
김용환 <yoway030@hotmail.com>
2025-12-31 13:18:07 +0900

    sample clean

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,10 +1,6 @@
-﻿using System.Numerics;
-
-using ELImGui;
-using ELImGui.Actor;
+﻿using ELImGui;
 using ELImGui.Effect;
 using ELImGui.Utils;
-using ELImGui.Widget;
 using ELImGui.Window;
 
 using Hexa.NET.ImGui;
@@ -16,112 +12,101 @@ internal class Program
     private static void Main(string[] args)
     {
         ImRenderer.CreateInstance();
-        ImRenderer visualizer = ImRenderer.Instance;
+        ImRenderer renderer = ImRenderer.Instance;
 
         ////////////////////////////////////////////////////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////////////////
-        ProcessMonitor processMonitor = new("ProcessMonitor");
+//        ProcessMonitor processMonitor = new("ProcessMonitor");
 
-        string jsonString =
-"""
-{           
-    "name": "John \"Johnny\" Smith",
-    "age": 32,
-    "email": null,
-    "isActive": true,
-    "roles": ["admin", "editor", "user"],
-    "profile": {
-    "address": {
-        "street": "123 Main St",
-        "city": "New York",
-        "zipcode": "10001"
-    },
-    "phone": "+1-800-555-0199"
-    },
-    "loginHistory": [
-    { "date": "2023-12-01T10:00:00Z", "ip": "192.168.1.1" },
-    { "date": "2023-12-05T14:22:13Z", "ip": "192.168.1.23" }
-    ]
-}
-""";
+//        string jsonString =
+//"""
+//{           
+//    "name": "John \"Johnny\" Smith",
+//    "age": 32,
+//    "email": null,
+//    "isActive": true,
+//    "roles": ["admin", "editor", "user"],
+//    "profile": {
+//    "address": {
+//        "street": "123 Main St",
+//        "city": "New York",
+//        "zipcode": "10001"
+//    },
+//    "phone": "+1-800-555-0199"
+//    },
+//    "loginHistory": [
+//    { "date": "2023-12-01T10:00:00Z", "ip": "192.168.1.1" },
+//    { "date": "2023-12-05T14:22:13Z", "ip": "192.168.1.23" }
+//    ]
+//}
+//""";
 
-        var tableRole = new DataTableRuleBuilder<PlayerRow>()
-            .AddColumn(name: "Name", width: 160,
-                getter: (in PlayerRow p) => p.Name,
-                renderer: (in PlayerRow p) => ImGuiHelper.TextUnformattedUntil(p.Name, ImGuiHelper.NewLineByte))
-            .AddColumn(
-                name: "Level",
-                width: 60,
-                getter: (in PlayerRow p) => p.Level.ToString(),
-                renderer: (in PlayerRow p) =>
-                {
-                    Identicon.RenderIdenticonRect(p.Name);
-                    ImGui.SameLine();
-                    ImGui.TextUnformatted(p.Level.ToString());
-                })
-            .AddColumn("DPS", 80, getter: (in PlayerRow p) => p.DPS.ToString())
-            .AddColumn("Class", 100, getter: (in PlayerRow p) => p.Class)
+        var tableRole = new DataTableRuleBuilder<TextRow>()
+            .AddColumn(name: "Time", getter: (in TextRow p) => p.Timestamp.ToString())
+            .AddColumn(name: "Index", width: 60, getter: (in TextRow p) => p.Index.ToString())
+            .AddColumn(name: "Text", width: 300, getter: (in TextRow p) => p.Text.ToString())
             .Build(
-                tooltipRender: (in PlayerRow row) =>
+                tooltipRender: (in TextRow row) =>
                 {
-                    ImGui.TextUnformatted($"Name: {row.Name}");
-                    ImGui.TextUnformatted($"Level: {row.Level}");
-                    ImGui.TextUnformatted($"DPS: {row.DPS}");
-                    ImGui.TextUnformatted($"Class: {row.Class}");
+                    ImGui.TextUnformatted($"Time: {row.Timestamp}");
+                    ImGui.TextUnformatted($"Index: {row.Index}");
+                    ImGui.TextUnformatted($"Text: {row.Text}");
                 },
-                getRowToString: (in PlayerRow row) =>
+                getRowToString: (in TextRow row) =>
                 {
-                    return $"{row.Name} {row.Level} {row.DPS} {row.Class}";
+                    return $"{row.Timestamp} {row.Index} {row.Text}";
                 });
-        IndexedTableWindow<PlayerRow> dataTable = new(tableRole, $"{"\U0001F3C4"}LogSurfer");
 
-        DataTableWindow<PlayerRow> ediTable = new(tableRole, "EdiTableWidget");
+        IndexedTableWindow<TextRow> indexedTable = new(tableRole, $"{"\U0001F3C4"}IndexedTableWindow");
+        indexedTable.Widget.AutoScroll = true;
 
-        SingleWidgetWindow<JsonWidget> jsonWidgetWindow = new("JsonWidgetWindow");
-        jsonWidgetWindow.InitializeWidget(new JsonWidget("JsonWidget", jsonWidgetWindow.WindowName));
-        jsonWidgetWindow.Widget.JsonText = jsonString;
+        DataTableWindow<TextRow> dataTable = new(tableRole, "DataTableWindow");
 
-        SingleWidgetWindow<TextViewWidget> textViewerWindow = new("TextViewerWindow");
-        textViewerWindow.InitializeWidget(new TextViewWidget("TextViewWidget", textViewerWindow.WindowName));
-        textViewerWindow.Widget.Initialize(jsonString, false);
+        //SingleWidgetWindow<JsonWidget> jsonWidgetWindow = new("JsonWidgetWindow");
+        //jsonWidgetWindow.InitializeWidget(new JsonWidget("JsonWidget", jsonWidgetWindow.WindowName));
+        //jsonWidgetWindow.Widget.JsonText = jsonString;
 
-        SingleWidgetWindow<SimpleTableWidget> simpleTableWindow = new("SimpleTableWindow");
-        simpleTableWindow.InitializeWidget(new SimpleTableWidget("SimpleTableWidget", simpleTableWindow.WindowName));
-        simpleTableWindow.Widget.Headers = new string[] { "asd", "asd1", "asd2" };
+        //SingleWidgetWindow<TextViewWidget> textViewerWindow = new("TextViewerWindow");
+        //textViewerWindow.InitializeWidget(new TextViewWidget("TextViewWidget", textViewerWindow.WindowName));
+        //textViewerWindow.Widget.Initialize(jsonString, false);
 
-        SingleWidgetWindow<CommandConsoleWidget> ConsoleWindow = new("ConsoleWindow");
-        ConsoleWindow.InitializeWidget(new CommandConsoleWidget("CommandConsoleWidget", ConsoleWindow.WindowName));
+        //SingleWidgetWindow<SimpleTableWidget> simpleTableWindow = new("SimpleTableWindow");
+        //simpleTableWindow.InitializeWidget(new SimpleTableWidget("SimpleTableWidget", simpleTableWindow.WindowName));
+        //simpleTableWindow.Widget.Headers = new string[] { "asd", "asd1", "asd2" };
 
-        CommandConsole console = new CommandConsole("CommandConsole");
-        console.InitializeWidget(new CommandConsoleWidget("CommandConsoleWidget", console.WindowName));
-        console.IsVisibleImObject = false;
-        console.Widget.InitSampleCommands();
+        //SingleWidgetWindow<CommandConsoleWidget> ConsoleWindow = new("ConsoleWindow");
+        //ConsoleWindow.InitializeWidget(new CommandConsoleWidget("CommandConsoleWidget", ConsoleWindow.WindowName));
 
-        VerticalMultiWidgetWindow multiWidgetWindow = new("MultiWidgetWindow");
-        var mmjson = new JsonWidget("JsonWidget1", multiWidgetWindow.WindowName);
-        mmjson.JsonText = jsonString;
-        multiWidgetWindow.AddWidget(mmjson, 0.5f);
+        //CommandConsole console = new CommandConsole("CommandConsole");
+        //console.InitializeWidget(new CommandConsoleWidget("CommandConsoleWidget", console.WindowName));
+        //console.IsVisibleImObject = false;
+        //console.Widget.InitSampleCommands();
 
-        var mmtext = new TextViewWidget("TextViewWidget", multiWidgetWindow.WindowName);
-        mmtext.Initialize(jsonString, false);
-        multiWidgetWindow.AddWidget(mmtext, 0.5f);
+        //VerticalMultiWidgetWindow multiWidgetWindow = new("MultiWidgetWindow");
+        //var mmjson = new JsonWidget("JsonWidget1", multiWidgetWindow.WindowName);
+        //mmjson.JsonText = jsonString;
+        //multiWidgetWindow.AddWidget(mmjson, 0.5f);
 
-        NodeViewer nodeView = new NodeViewer("NodeViewer");
+        //var mmtext = new TextViewWidget("TextViewWidget", multiWidgetWindow.WindowName);
+        //mmtext.Initialize(jsonString, false);
+        //multiWidgetWindow.AddWidget(mmtext, 0.5f);
 
-        visualizer.RenderActionQueue.Post((context) =>
+        //NodeViewer nodeView = new NodeViewer("NodeViewer");
+
+        renderer.RenderActionQueue.Post((context) =>
         {
             var windows = new BaseWindow[]
             {
+                indexedTable,
                 dataTable,
-                ediTable,
-                processMonitor,
-                console,
-                nodeView,
-                jsonWidgetWindow,
-                textViewerWindow,
-                simpleTableWindow,
-                ConsoleWindow,
-                multiWidgetWindow,
+                //processMonitor,
+                //console,
+                //nodeView,
+                //jsonWidgetWindow,
+                //textViewerWindow,
+                //simpleTableWindow,
+                //ConsoleWindow,
+                //multiWidgetWindow,
             };
 
             foreach (var window in windows)
@@ -130,46 +115,43 @@ internal class Program
             }
         });
 
-        visualizer.RenderActionQueue.Post((context) =>
+        renderer.RenderActionQueue.Post((context) =>
         {
             context.ForegroundEffects.Add(
                 new HexagonOverlayEffect(
                     DateTime.UtcNow,
-                    DateTime.UtcNow.AddSeconds(1.5),
+                    DateTime.UtcNow.AddSeconds(10),
                     new TimeSpan(0, 0, 0),
                     new TimeSpan(5500000),
-                    new string[] { "EL", "Server", "On", "Your", "Mark" },
-                    ImGuiStyleSet.White, ImGuiColorHelper.BrightenClamped(ImGuiStyleSet.Blue, 0.3f)));
+                    new string[] { "On", "Your", "Mark" },
+                    ImGuiColorHelper.White, ImGuiColorHelper.BrightenClamped(ImGuiColorHelper.Blue, 0.3f)));
         });
 
         ////////////////////////////////////////////////////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////////////////
         ///
 
-        ImRenderListActor<string> renderListActor = new();
-        var list = renderListActor.GetPostAdapter();
+        //ImRenderListActor<string> renderListActor = new();
+        //var list = renderListActor.GetPostAdapter();
 
-        ImRenderDictionaryActor<int, string> renderDictActor = new();
-        var dict = renderDictActor.GetPostAdapter();
+        //ImRenderDictionaryActor<int, string> renderDictActor = new();
+        //var dict = renderDictActor.GetPostAdapter();
 
         // 스레드 생성 및 시작
         Thread thread = new Thread(() =>
         {
-            ediTable.Widget.InitializeActor(Environment.CurrentManagedThreadId);
-            renderListActor.Initialize(Environment.CurrentManagedThreadId);
-            renderDictActor.Initialize(Environment.CurrentManagedThreadId);
+            dataTable.Widget.InitializeActor(Environment.CurrentManagedThreadId);
 
-            visualizer.Initialize("Sample");
+            renderer.Initialize("Sample");
 
-            while (visualizer.IsWindowShouldClose == false)
+            while (renderer.IsWindowShouldClose == false)
             {
-                renderListActor.Work();
-                renderDictActor.Work();
-                visualizer.Loop();
+                renderer.Loop();
             }
 
-            visualizer.Cleanup();
+            renderer.Cleanup();
         });
+
         thread.Start();
 
         ////////////////////////////
@@ -206,56 +188,82 @@ internal class Program
         //});
 
         //////////////////////////
+        ///
 
-
-        var taskAddData = Task.Run(async () =>
+        var taskDataTable = Task.Run(async () =>
         {
-
-            Random random = new Random();
-            int logIndex = 0;
-            while (visualizer.IsWindowShouldClose == false)
+            while (renderer.IsWindowShouldClose == false)
             {
-                dataTable.PushData(new PlayerRow { Name = $"{logIndex}AAAAAAAA😀AAAAAAAAAAAAAAA사나A", Level = logIndex, Class = "EEEE", DPS = 10 });
-
-                if (logIndex % 5 == 0)
+                try
                 {
-                    ediTable.AddDataPost(new PlayerRow
+                    var lines = File.ReadAllLines("data.Text");
+                    for (int i = 0; i < lines.Length; i++)
                     {
-                        Name = $"Player_{logIndex}",
-                        Level = random.Next(1, 100),
-                        Class = "Warrior",
-                        DPS = (float)(random.NextDouble() * 1000),
+                        indexedTable.PushData(new TextRow
+                        {
+                            Timestamp = DateTime.UtcNow,
+                            Index = i,
+                            Text = lines[i]
+                        });
+
+                        await Task.Delay(100);
+                    }
+                }
+                catch
+                {
+                    indexedTable.PushData(new TextRow
+                    {
+                        Timestamp = DateTime.UtcNow,
+                        Index = 0,
+                        Text = "failed file read"
                     });
                 }
-                else if (logIndex % 7 == 0)
-                {
-                    ediTable.UpdateDataAtPost((logIndex / 10), new PlayerRow
-                    {
-                        Name = $"UpdatedPlayer_{logIndex}",
-                        Level = random.Next(1, 100),
-                        Class = "Mage",
-                        DPS = (float)(random.NextDouble() * 1000),
-                    });
-                }
-                else if (logIndex % 11 == 0)
-                {
-                    ediTable.RemoveDataAtPost(0);
-                }
-
-                await Task.Delay(100);
-                logIndex++;
             }
         });
+
+        
+
+
+        //var taskAddData = Task.Run(async () =>
+        //{
+
+        //    Random random = new Random();
+        //    int logIndex = 0;
+        //    while (renderer.IsWindowShouldClose == false)
+        //    {
+        //        dataTable.PushData(new SampleDataType { Name = $"{logIndex}AAAAAAAA😀AAAAAAAAAAAAAAA사나A", Level = logIndex, Class = "EEEE", DPS = 10 });
+
+        //        if (logIndex % 5 == 0)
+        //        {
+        //            ediTable.AddDataPost(new SampleDataType
+        //            {
+        //                Name = $"Player_{logIndex}",
+        //                Level = random.Next(1, 100),
+        //                Class = "Warrior",
+        //                DPS = (float)(random.NextDouble() * 1000),
+        //            });
+        //        }
+        //        else if (logIndex % 7 == 0)
+        //        {
+        //            ediTable.UpdateDataAtPost((logIndex / 10), new SampleDataType
+        //            {
+        //                Name = $"UpdatedPlayer_{logIndex}",
+        //                Level = random.Next(1, 100),
+        //                Class = "Mage",
+        //                DPS = (float)(random.NextDouble() * 1000),
+        //            });
+        //        }
+        //        else if (logIndex % 11 == 0)
+        //        {
+        //            ediTable.RemoveDataAtPost(0);
+        //        }
+
+        //        await Task.Delay(100);
+        //        logIndex++;
+        //    }
+        //});
 
 
         thread.Join();
     }
-}
-
-public sealed class PlayerRow
-{
-    public string Name { get; init; } = "";
-    public int Level { get; init; }
-    public float DPS { get; init; }
-    public string Class { get; init; } = "";
 }
---
+++ Sample/Types.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample;
+
+internal class TextRow
+{
+    public DateTime Timestamp { get; set; } = DateTime.MinValue;
+    public int Index { get; set; } = 0;
+    public string Text { get; set; } = string.Empty;
+
+}
+
+public sealed class SampleDataType
+{
+    public string Name { get; init; } = "";
+    public int Level { get; init; }
+    public float DPS { get; init; }
+    public string Class { get; init; } = "";
+}
